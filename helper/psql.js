53c8f9962294077dd3f45edf42320221557a630f
MBorne <mborne@users.noreply.github.com>
2018-11-09 12:48:16 +0100

    restore helper/psql.js

---
+++ helper/psql.js
@@ -0,0 +1,57 @@
+const shell = require('shelljs');
+
+const _ = require('lodash');
+
+const debug = require('debug')('psql');
+
+/**
+ * Run psql on a given file
+ * @param {Object} options
+ * @param {String} options.inputPath
+ * @param {Boolean} [options.quiet=true]
+ * @param {Boolean} [options.singleTransation=true]
+ * @return {Promise}
+ */
+var psql = function(options){
+    var options = _.defaults(options,{
+        quiet: true,
+        singleTransation: true
+    });
+
+    return new Promise(function(resolve,reject){
+        if (!shell.which('psql')) {
+            reject({
+                'status': 'error',
+                'message': 'psql not found'
+            });
+        }
+        var commandParts = [];
+        commandParts.push('psql');
+        if ( options.quiet ){
+            commandParts.push('--quiet');
+        }
+        if ( options.singleTransation ){
+            commandParts.push('--single-transaction');
+        }
+        commandParts.push('-f '+options.inputPath);
+
+        var command = commandParts.join(' ');
+        debug(command);
+        if (shell.exec(command).code !== 0) {
+            reject({
+                'status': 'error',
+                'message': 'Fail to import '+options.inputPath,
+                'command': command
+            });
+        }else{
+            resolve({
+                'status': 'success',
+                'message': 'File imported : '+options.inputPath,
+                'command': command
+            });
+        }
+    });
+};
+
+module.exports = psql;
+
