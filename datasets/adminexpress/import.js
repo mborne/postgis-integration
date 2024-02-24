7bcb9ef069c6d9d307824e960fc425ac8b8d755e
mborne <mborne@users.noreply.github.com>
2024-02-24 20:52:54 +0100

    feat(adminexpress): mise à jour

--- datasets/adminexpress/import.js
+++ datasets/adminexpress/import.js
@@ -1,16 +1,17 @@
-const {Database,SourceManager} = require('@mborne/postgis-helper');
+const { Database, SourceManager } = require('@mborne/postgis-helper');
 const download = require('@mborne/dl');
 const ogr2pg = require('@mborne/ogr2pg');
 const extract = require('@mborne/extract');
 const DataDir = require('../../helper/DataDir');
+const path = require('path');
 
 const config = require('./config.json');
 const SCHEMA_NAME = 'adminexpress';
 
-async function main(){
+async function main() {
     var database = await Database.createDatabase();
     /* Import schema */
-    await database.batch(__dirname+'/sql/schema.sql');
+    await database.batch(__dirname + '/sql/schema.sql');
 
     /* Prepare local directory */
     var datasetDir = await DataDir.createDataDir(SCHEMA_NAME);
@@ -18,7 +19,7 @@ async function main(){
     /* Download archive */
     let archivePath = await download({
         sourceUrl: config.url,
-        targetPath: datasetDir.getPath()+'/ADMIN-EXPRESS.7z'
+        targetPath: datasetDir.getPath() + '/ADMIN-EXPRESS.7z'
     });
 
     /* Extract archive */
@@ -26,49 +27,35 @@ async function main(){
         archivePath: archivePath
     });
 
-
-    /* group shapefiles by table */
-    var shapefiles = {
-        "REGION": [],
-        "DEPARTEMENT": [],
-        "ARRONDISSEMENT_DEPARTEMENTAL": [],
-        "EPCI": [],
-        "COMMUNE": [],
-        "CHEF_LIEU": []
-    };
-    datasetDir.getFiles().filter(function(file) {
-        for ( var tableName in shapefiles ){
-            if ( file.endsWith(tableName+'.shp') ){
-                shapefiles[tableName].push(file);
-            }
-        }
-    });
-
     /* import shapefiles */
     var tasks = [];
-    for ( var tableName in shapefiles ){
-        var tableShapefiles = shapefiles[tableName];
-        tableShapefiles.forEach(function(tableShapefile){
-            tasks.push(ogr2pg({
-                inputPath: tableShapefile,
-                schemaName: SCHEMA_NAME,
-                tableName: tableName,
-                promoteToMulti: true
-            }));
-        });
+
+    for (const file of datasetDir.getFiles()) {
+        if (!file.endsWith('.shp')) {
+            continue;
+        }
+        const tableName = path.parse(file).name;
+        tasks.push(ogr2pg({
+            inputPath: file,
+            schemaName: SCHEMA_NAME,
+            tableName: tableName,
+            promoteToMulti: true,
+            createTable: true
+        }));
     }
+
     await Promise.all(tasks);
 
     /* Save source */
-    let sourceManager = await SourceManager.createSourceManager(database,SCHEMA_NAME);
-	await sourceManager.add(config);
+    let sourceManager = await SourceManager.createSourceManager(database, SCHEMA_NAME);
+    await sourceManager.add(config);
 
     /* cleanup directory */
     datasetDir.remove();
     await database.close();
 }
 
-main().catch(function(err){
+main().catch(function (err) {
     console.log(err);
     process.exit(1);
 });
